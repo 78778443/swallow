8de97c61b2f52d905fb873b232e511aa9b3c1a33
王重力 <gravity_wang@163.com>
2024-05-13 20:51:00 +0800

    bishe v3

--- docker/docker_build.sh
+++
@@ -1,8 +0,0 @@
-php ./daochuSql.php
-
-docker build -t daxia/swallow:latest .
-docker push daxia/swallow:latest
-
-docker build -f DockerfileMysql -t daxia/swallow:mysql57 .
-docker push daxia/swallow:mysql57
-
--- docker/start.sh
+++
@@ -1,8 +0,0 @@
-cd /root/code && git clone --depth=1 https://gitee.com/songboy/swallow.git
-cd /root/code/swallow && git fetch && git reset --hard origin/master
-chmod -R 777 /root/code/swallow
-cd /root/code/swallow && cp .example.env .env && composer install
-/usr/sbin/php-fpm7.4 -R
-/usr/sbin/nginx
-
-sleep 10000000000
